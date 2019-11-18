18c2a370c3f5f39653e18eca00288a843ff1f37d
eroak <mr.eroak@gmail.com>
2019-11-18 21:54:32 +0100

    Attributes are now optional

--- projects/n-orm/src/lib/interfaces/associations.ts
+++ projects/n-orm/src/lib/interfaces/associations.ts
@@ -1,6 +1,6 @@
 export interface IAssociation {
-  type: 'OneToMany' | 'OneToOne' | 'ManyToMany';
-  propertyName: string;
+  type?: 'OneToMany' | 'OneToOne' | 'ManyToMany';
+  propertyName?: string;
 
   targetEntity?: object; // Group
   mappingEntity?: object; // UserGroup
@@ -13,20 +13,20 @@ export interface IAssociation {
 export interface IOneToOne extends IAssociation {
   targetEntity: object;
   joinPropertyName: string;
-  referencedPropertyName: string;
+  referencedPropertyName?: string;
 }
 
 export interface IOneToMany extends IAssociation {
   targetEntity: object; // InvoiceLines
   joinPropertyName: string; // InvoiceID
-  referencedPropertyName: string; // ID (default)
+  referencedPropertyName?: string; // ID (default)
 }
 
 export interface IManyToMany extends IAssociation {
   targetEntity: object; // Group
     mappingEntity: object; // UserGroup
   joinPropertyName: string; // UserID
-  referencedPropertyName: string; // ID (default)
+  referencedPropertyName?: string; // ID (default)
     inverseJoinPropertyName: string; // GroupID
-    inverseReferencedPropertyName: string; // ID (default)
+    inverseReferencedPropertyName?: string; // ID (default)
 }
