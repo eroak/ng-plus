85191eee9935be92d3e527a62da78c8d78226504
eroak <mr.eroak@gmail.com>
2019-11-20 21:35:51 +0100

    Parameter ID is now optionnal

--- projects/n-orm/src/lib/interfaces/data-accessor.ts
+++ projects/n-orm/src/lib/interfaces/data-accessor.ts
@@ -4,7 +4,7 @@ export interface DataAccessor {
 
   get<T>(resourceName: string, ID: number | string): Observable<T>;
   list<T>(resourceName: string, filters?: any): Observable<Array<T>>;
-  save<T>(resourceName: string, entity: any, ID: string | number | null): Observable<T>;
+  save<T>(resourceName: string, entity: any, ID?: string | number | null): Observable<T>;
   delete(resourceName: string, ID: number | string): Observable<any>;
 
 }
